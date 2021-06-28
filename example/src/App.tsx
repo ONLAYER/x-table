ca7bfd88596ac1d614d3443273f5b384b95cf093
vserifsaglam <vahit.serif119@gmail.com>
2021-06-28 16:41:46 +0300

    made some updates

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import XTable from "../../src/XTable";
 import React from "react";
 
 const App = () => {
-  return <XTable  data={[{id: 1}]} />
+  return <XTable headCells={[]}  data={[{id: 1}]} />
 }
 
 export default App
